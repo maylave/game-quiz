59c06bcc674895184b1c8a4e06974e38fd9f3744
maylave <maylavelave@gmail.com>
2026-05-06 21:38:23 +0300

    Update vite.config.ts23

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [vue(), vueDevTools(), tailwindcss()],
-	base: '/',
+	 base: '/game-quiz/',
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url))
