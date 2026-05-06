23bcffad65ee9e895bdb8817222675e8f165e65d
Artem <prrpappspl@gmail.com>
2026-05-06 22:01:47 +0300

    ok

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,12 @@ import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
-export default defineConfig({
-	plugins: [vue(), vueDevTools(), tailwindcss()],
-	base: '/game-quiz/',
+export default defineConfig(({ mode }) => ({
+	plugins: [vue(), mode === 'development' && vueDevTools(), tailwindcss()],
+	base: '/game-quiz',
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url))
 		}
 	}
-})
+}))
