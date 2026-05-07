cfc80d03177d800542b07b37c0e9250c12c180bc
maylave <prrpappspl@gmail.com>
2026-05-07 22:18:34 +0300

    update html

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
 	plugins: [vue(), mode === 'development' && vueDevTools(), tailwindcss()],
-	base: '/game-quiz',
+	base: '/',
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url))
